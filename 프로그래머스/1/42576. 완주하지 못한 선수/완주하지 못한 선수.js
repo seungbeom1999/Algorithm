a517e6171dc49d7c6d291dc660527699b0d45678
seungbeom1999 <121495648+seungbeom1999@users.noreply.github.com>
2023-11-06 14:40:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.71 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,17 +1,3 @@
-function  solution(participant, completion)  {
-    const map = new Map();
 
-    for(let i = 0; i < participant.length; i++) {
-        let a = participant[i], 
-            b = completion[i];
-
-        map.set(a, (map.get(a) || 0) + 1);
-        map.set(b, (map.get(b) || 0) - 1);
-    }
-    
-    for(let [k, v] of map) {
-        if(v > 0) return k;
-    }
-    
-    return 'nothing';
-}
+2
+var solution=(_,$)=>_.find(_=>!$[_]--,$.map(_=>$[_]=($[_]|0)+1))
